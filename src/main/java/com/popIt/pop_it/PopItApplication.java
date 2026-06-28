705a382228ef9dee471b3059ac945adae970f7da
[timerrrr] <[sichan0304@naver.com]>
2026-06-28 16:59:31 +0900

    [chore (시치(송시찬)): Spring Boot 프로젝트 초기 세팅 ]

---
+++ src/main/java/com/popIt/pop_it/PopItApplication.java
@@ -0,0 +1,13 @@
+package com.popIt.pop_it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/popIt/pop_it/PopItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popIt.pop_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
