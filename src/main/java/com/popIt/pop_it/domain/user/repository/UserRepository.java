04daf3a8d41155536e2ab898167842431c8200d2
Yujin Kim <yujii22001@gmail.com>
2026-07-07 19:39:45 +0900

    feat: 코드 리뷰 반영

--- src/main/java/com/popIt/pop_it/domain/user/repository/UserRepository.java
+++ src/main/java/com/popIt/pop_it/domain/user/repository/UserRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<Object> findBySocialProviderAndSocialUid(SocialProvider socialProvider, String socialUid);
+    Optional<User> findBySocialProviderAndSocialUid(SocialProvider socialProvider, String socialUid);
 }
