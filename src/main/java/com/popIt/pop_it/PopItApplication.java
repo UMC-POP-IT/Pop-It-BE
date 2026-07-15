01726d8312f460c6ddb20735ea87893ff31ab42d
Harim Kim <hrkim0529@naver.com>
2026-07-16 01:33:22 +0900

    Feat: presigned URL 발급 API 구현 (#36)
    * chore: AWS SDK v2 S3 의존성 추가
    
    * feat: AWS 설정 프로퍼티 구성 추가
    
    * feat: S3Presigner 빈 등록
    
    * feat: presigned URL 발급 API 구현
    
    * refactor: 비공개 버킷 추가를 위해 UploadType을 SPACE_IMAGE로 축소
    
    * refactor: 파일 목록 크기 제한 추가
    
    * refactor: 미사용 코드 제거 및 업로드 파일 타입 명시
    
    * fix: UploadType의 SPACE_IMAGE로의 축소로 인한 허용 값 수정
    
    * merge: develop 병합 충돌 해결

--- src/main/java/com/popIt/pop_it/PopItApplication.java
+++ src/main/java/com/popIt/pop_it/PopItApplication.java
@@ -1,8 +1,11 @@
 package com.popIt.pop_it;
 
+import com.popIt.pop_it.global.config.AwsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(AwsProperties.class)
 @SpringBootApplication
 public class PopItApplication {
 
