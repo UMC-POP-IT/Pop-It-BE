c7d6c8ba29f69a27fcfbbab7623ffde21d4a7aae
김하림 <hrkim0529@naver.com>
2026-07-20 00:46:23 +0900

    refactor: FacilityDataInitializer 로깅 방식 변경 (println -> Slf4j)

--- src/main/java/com/popIt/pop_it/domain/facility/init/FacilityDataInitializer.java
+++ src/main/java/com/popIt/pop_it/domain/facility/init/FacilityDataInitializer.java
@@ -4,6 +4,7 @@ import com.popIt.pop_it.domain.facility.entity.Facility;
 import com.popIt.pop_it.domain.facility.enums.FacilityName;
 import com.popIt.pop_it.domain.facility.repository.FacilityRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class FacilityDataInitializer implements ApplicationRunner {
@@ -31,6 +33,6 @@ public class FacilityDataInitializer implements ApplicationRunner {
                 .toList();
 
         facilityRepository.saveAll(facilities);
-        System.out.println(">>> [FacilityInit] 시설 " + facilities.size() + "개 삽입 완료");
+        log.info("시설 {}개 삽입 완료", facilities.size());
     }
 }
