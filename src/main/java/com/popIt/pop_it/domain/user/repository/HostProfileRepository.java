e93fd787b596f4ad1a751fb2504847d363b53d07
김하림 <hrkim0529@naver.com>
2026-07-21 17:17:11 +0900

    feat: 호스트 프로필 조회 메서드 추가

--- src/main/java/com/popIt/pop_it/domain/user/repository/HostProfileRepository.java
+++ src/main/java/com/popIt/pop_it/domain/user/repository/HostProfileRepository.java
@@ -4,6 +4,8 @@ import com.popIt.pop_it.domain.user.entity.HostProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HostProfileRepository extends JpaRepository<HostProfile, Long> {
 
@@ -12,4 +14,7 @@ public interface HostProfileRepository extends JpaRepository<HostProfile, Long> 
 
     // 동일 사업자등록번호로 다른 사용자가 중복 등록하는지 해시로 선검사
     boolean existsByBusinessRegistrationNumberHash(String businessRegistrationNumberHash);
+
+    // 공간 등록 시 호스트 권한 확인 및 space.host_id 획득용
+    Optional<HostProfile> findByUserId(Long userId);
 }
