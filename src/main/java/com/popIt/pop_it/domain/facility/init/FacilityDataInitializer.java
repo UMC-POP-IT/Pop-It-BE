b6716494863c1ec87eff7b44416e66bc6b80b45a
김하림 <hrkim0529@naver.com>
2026-07-22 12:16:21 +0900

    chore: 머지 충돌 병합

--- src/main/java/com/popIt/pop_it/domain/facility/init/FacilityDataInitializer.java
+++ src/main/java/com/popIt/pop_it/domain/facility/init/FacilityDataInitializer.java
@@ -4,10 +4,7 @@ import com.popIt.pop_it.domain.facility.entity.Facility;
 import com.popIt.pop_it.domain.facility.enums.FacilityName;
 import com.popIt.pop_it.domain.facility.repository.FacilityRepository;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
-=======
 import lombok.extern.slf4j.Slf4j;
->>>>>>> develop
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -15,10 +12,7 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 
-<<<<<<< HEAD
-=======
 @Slf4j
->>>>>>> develop
 @Component
 @RequiredArgsConstructor
 public class FacilityDataInitializer implements ApplicationRunner {
@@ -39,10 +33,6 @@ public class FacilityDataInitializer implements ApplicationRunner {
                 .toList();
 
         facilityRepository.saveAll(facilities);
-<<<<<<< HEAD
-        System.out.println(">>> [FacilityInit] 시설 " + facilities.size() + "개 삽입 완료");
-=======
         log.info("시설 {}개 삽입 완료", facilities.size());
->>>>>>> develop
     }
 }
