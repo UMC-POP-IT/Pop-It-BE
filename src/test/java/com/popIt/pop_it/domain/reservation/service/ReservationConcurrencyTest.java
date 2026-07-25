b4af0ecf268a39969410c938ec4c146d404f8463
김하림 <hrkim0529@naver.com>
2026-07-25 19:01:46 +0900

    test: 테스트 코드에 Space 생성 시 누락된 dong 필드 추가

--- src/test/java/com/popIt/pop_it/domain/reservation/service/ReservationConcurrencyTest.java
+++ src/test/java/com/popIt/pop_it/domain/reservation/service/ReservationConcurrencyTest.java
@@ -67,6 +67,7 @@ public class ReservationConcurrencyTest {
                 .longitude(127.0)
                 .roadAddress("테스트로 1")
                 .addressDetail("101호")
+                .dong("합정동")
                 .deposit(1_000_000L)
                 .pricePerDay(100_000)
                 .availableStartDate(LocalDate.now())
