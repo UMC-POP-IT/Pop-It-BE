7b823adc0c46e44dcea2355d10412764c14af838
Yujin Kim <yujii22001@gmail.com>
2026-08-05 00:25:57 +0900

    feat: 스웨거 태그 노출 순서 정렬

--- src/main/java/com/popIt/pop_it/global/config/SwaggerConfig.java
+++ src/main/java/com/popIt/pop_it/global/config/SwaggerConfig.java
@@ -6,13 +6,35 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.Comparator;
+import java.util.List;
+
 @Configuration
 @Profile("!prod")
 public class SwaggerConfig {
+
+    // Swagger UI 태그 노출 순서 지정
+    private static final List<String> TAG_ORDER = List.of(
+            "인증",
+            "사용자",
+            "호스트",
+            "시설",
+            "공간",
+            "공간 찜",
+            "공간 3D 큐레이션 - 씬",
+            "공간 3D 큐레이션 - 핫스팟",
+            "예약",
+            "본인인증",
+            "계약",
+            "결제",
+            "업로드"
+    );
 
     @Bean
     public OpenAPI swagger() {
@@ -34,5 +56,23 @@ public class SwaggerConfig {
                 .addServersItem(new Server().url("/"))
                 .addSecurityItem(securityRequirement)
                 .components(components);
+    }
+
+    // OpenApiCustomizer: 스웨거 문서가 화면에 그려지기 바로 전 단계에서 실행되는 최종 커스텀 필터
+    @Bean
+    public OpenApiCustomizer tagOrderCustomizer() {
+        return openApi -> {
+            if (openApi.getTags() == null) {
+                return;
+            }
+            // 태그 순서 정렬
+            List<Tag> sorted = openApi.getTags().stream()
+                    .sorted(Comparator.comparingInt(tag -> {
+                        int index = TAG_ORDER.indexOf(tag.getName());
+                        return index == -1 ? Integer.MAX_VALUE : index;
+                    }))
+                    .toList();
+            openApi.setTags(sorted);
+        };
     }
 }
