522ec75fde74dbe8d3d46125d5467bc91e87daca
seoyeon5117 <seoyun020207@gmail.com>
2026-08-06 15:21:27 +0900

    feat: 운영 Swagger authorize 설정

--- src/main/java/com/popIt/pop_it/global/config/SwaggerConfig.java
+++ src/main/java/com/popIt/pop_it/global/config/SwaggerConfig.java
@@ -8,10 +8,8 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!prod")
 public class SwaggerConfig {
 
     @Bean
