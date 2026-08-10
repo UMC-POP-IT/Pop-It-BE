e1efd972de3b79c2be20b27ab7b48e359181226a
김하림 <hrkim0529@naver.com>
2026-08-10 17:54:21 +0900

    fix: 보증금 하한 미포함 문제 해결

--- src/main/java/com/popIt/pop_it/domain/space/dto/SpaceReqDTO.java
+++ src/main/java/com/popIt/pop_it/domain/space/dto/SpaceReqDTO.java
@@ -75,6 +75,7 @@ public class SpaceReqDTO {
 
             @Schema(description = "보증금 (최대 1,000,000원)", example = "450000")
             @NotNull
+            @Min(value = 0, message = "보증금은 0원 이상이어야 합니다.")
             @Max(value = 1_000_000, message = "보증금은 1,000,000원 이하이어야 합니다.")
             Long deposit,
 
